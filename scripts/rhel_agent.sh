5ddaa02f75db7320efa898fff74f667c9e948f99
Alen Komljen <alen.komljen@live.com>
2014-04-27 09:53:47 +0200

    Minor changes and improvements

--- scripts/rhel_agent.sh
+++ scripts/rhel_agent.sh
@@ -4,6 +4,7 @@
 # SecureServer
 #
 #===============================================================================
+endpoint="https://test.com"
 
 os_name=$(awk -F ' release ' '{print $1}' /etc/redhat-release)
 os_release=$(awk -F ' release ' '{print $2}' /etc/redhat-release | awk '{print $1}')
@@ -11,21 +12,25 @@ os_release=$(awk -F ' release ' '{print $2}' /etc/redhat-release | awk '{print $
 hostname=$(hostname)
 
 IFS=$'\n'
-pkg_upgrade=$(yum list updates -q | grep -v "Updated Packages") || true
+pkg_upgrade=$(yum list updates -q | grep -v "Updated Packages")
 pkg_installed=$(rpm -qa)
 packages=()
 
 for pkg in $pkg_upgrade
 do
-    name=$(echo $pkg | cut -d'.' -f1 )
-    version=$(echo "${pkg_installed}" | grep "^${name}-[0-9]" | sed "s/^${name}-//g" | sed 's/.[^.]*$//g')
-    version_update=$(echo ${pkg} | awk '{print $2}')
+    name=$(echo "$pkg" | cut -d'.' -f1 )
+    version=$(echo "$pkg_installed" | grep "^${name}-[0-9]" | sed -e "s/^${name}-//g" -e "s/.[^.]*$//g")    
+    version_update=$(echo "$pkg" | awk '{print $2}')
 
     packages+=$(printf '{"name":"%s","version":"%s","version_update":"%s"},' \
                          "$name" "$version" "$version_update")
 done
 
-packages=$(sed 's/},$/}/g' <<< $packages)
+packages=$(sed 's/},$/}/g' <<< "$packages")
 
-printf '{"os_name":"%s","os_release":"%s","os_codename":"%s","hostname":"%s","packages":[%s]}\n' \
-         "$os_name" "$os_release" "$os_codename" "$hostname" "$packages"
+response=$(printf '{"os_name":"%s","os_release":"%s","os_codename":"%s","hostname":"%s","packages":[%s]}\n' \
+                    "$os_name" "$os_release" "$os_codename" "$hostname" "$packages")
+
+#curl -d "$response" -i "$endpoint"
+
+echo "$response"
--- scripts/ubuntu_agent.sh
+++ scripts/ubuntu_agent.sh
@@ -4,7 +4,7 @@
 # SecureServer
 #
 #===============================================================================
-set -e
+endpoint="https://test.com"
 
 os_name=$(grep DISTRIB_ID /etc/*release | cut -d= -f2)
 os_release=$(grep DISTRIB_RELEASE /etc/*release | cut -d= -f2)
@@ -12,20 +12,24 @@ os_codename=$(grep DISTRIB_CODENAME /etc/*release | cut -d= -f2)
 hostname=$(hostname)
 
 IFS=$'\n'
-pkg_upgrade=$(apt-get upgrade -s | grep ^Inst) || true
+pkg_upgrade=$(apt-get upgrade -s | grep ^Inst)
 packages=()
 
 for pkg in $pkg_upgrade
 do
-    name=$(echo $pkg | cut -d' ' -f2)
-    version=$(echo $pkg | cut -d'[' -f2 | cut -d']' -f1)
-    version_update=$(echo $pkg | awk -F'[()]' '{print $2}' | cut -d' ' -f1)
+    name=$(echo "$pkg" | awk '{print $2}')
+    version=$(echo "$pkg" | cut -d'[' -f2 | cut -d']' -f1)
+    version_update=$(echo "$pkg" | awk -F'[()]' '{print $2}' | cut -d' ' -f1)
 
     packages+=$(printf '{"name":"%s","version":"%s","version_update":"%s"},' \
                          "$name" "$version" "$version_update")
 done
 
-packages=$(sed 's/},$/}/g' <<< $packages)
+packages=$(sed 's/},$/}/g' <<< "$packages")
 
-printf '{"os_name":"%s","os_release":"%s","os_codename":"%s","hostname":"%s","packages":[%s]}\n' \
-         "$os_name" "$os_release" "$os_codename" "$hostname" "$packages"
+response=$(printf '{"os_name":"%s","os_release":"%s","os_codename":"%s","hostname":"%s","packages":[%s]}\n' \
+                    "$os_name" "$os_release" "$os_codename" "$hostname" "$packages")
+
+#curl -d "$response" -i "$endpoint"
+
+echo "$response"
